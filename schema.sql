1752eebfb53a1b7597d5310a25c88888ffdfaa59
fpestunov <fpestunov@gmail.com>
2019-09-08 21:15:20 +0400

    add database schema and queries

---
+++ schema.sql
@@ -0,0 +1,72 @@
+DROP DATABASE IF EXISTS `giftube`;
+
+CREATE DATABASE `giftube`
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+USE `giftube`;
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+    `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `dt_add` datetime,
+  `user_id` int(11) unsigned NOT NULL,
+  `gif_id` int(11) unsigned NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `gifs`;
+CREATE TABLE `gifs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `dt_add` datetime,
+  `show_count` int(11) unsigned NOT NULL DEFAULT '0',
+  `like_count` int(11) NOT NULL DEFAULT '0',
+  `fav_count` int(11) NOT NULL DEFAULT '0',
+  `title` char(255) NOT NULL,
+  `description` text,
+  `path` char(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `gifs_fav`;
+CREATE TABLE `gifs_fav` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `gif_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `gifs_like` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `gif_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `gif_id` (`gif_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `dt_add` datetime,
+  `email` char(255) NOT NULL,
+  `name` char(255) NOT NULL,
+  `password` char(64) DEFAULT NULL,
+  `avatar_path` char(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `gifs_like` ADD UNIQUE INDEX (`user_id`, `gif_id`);
+ALTER TABLE `gifs_fav` ADD UNIQUE INDEX (`user_id`, `gif_id`);
